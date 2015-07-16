f6ace5d0599af2344caca02153c069f08b542ebc
kalimist123 <kali.mist@gmail.com>
2015-07-16 10:35:58 +0100

    Update hellowworld.js

--- js/hellowworld.js
+++ js/hellowworld.js
@@ -1,1 +1,22 @@
 
+define(["coreJS/adapt", "templates"], function(Adapt) {
+    
+    Adapt.register('helloworld', 'view', {
+        
+        el: "#wrapper",
+        
+        initialize: function() {
+            this.render();
+        },
+        
+        render: function() {
+            var data = {"title":"Hello World"};
+            var template = Handlebars.templates["helloworld"];
+            this.$el.append(template(data));
+            return this;
+        }
+        
+    });
+    
+    
+})
